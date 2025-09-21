75c1b3234ad160ef5040e0d3f31b05ef99b6507d
Furkan <furkancelebiii9@gmail.com>
2025-09-21 12:25:54 +0300

    İlk commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('./routes');
 const path = require('path');
-
+// --
 const app = express();
 app.use(helmet());
 app.use(cors());
